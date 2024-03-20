4da9ba2c94cafc691933e170f05eb5448f5286b4
Hallocoos <hallocoos@DESKTOP-2HQ1EII>
2024-03-20 14:22:59 +0200

    small changes to the pipeline;

--- tests/tests_views.py
+++ tests/tests_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class MyViewTests(TestCase):
     def test_my_view(self):
-        response = self.client.get(reverse('my_view_url'))
+        response = self.client.get(reverse("my_view_url"))
         self.assertEqual(response.status_code, 200)
